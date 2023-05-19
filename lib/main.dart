1d66c2a3486414afec75a58f0d4a6c8e243abae8
Esraa <esraahassan967@gmail.com>
2023-05-19 22:58:24 +0300

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/shared/styles/my_theme.dart';
 
+import 'firebase_options.dart';
 import 'home_layout/home_layout.dart';
 
-void main(){
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApplication());
 }
 
